a15c44b6fd59300d1adc6e0946892e43660ca8b0
Mishailian <michailbond77@gmail.com>
2025-02-18 03:01:32 +0500

    .

--- config.lua
+++ config.lua
@@ -171,5 +171,15 @@ lvim.plugins = {
             ]])
     end,
   },
+  -- Marckdown prev
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 
 }
